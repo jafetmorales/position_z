e40e671af6033abffdd474e39e3981cc6fa01519
Jafet Morales <jafet.aaron.morales@gmail.com>
2019-06-24 21:38:25 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('zinga_position_z', {
+    schema: {
+    speed: {type: 'number', default: -.005}
+  },
+  init: function () {
+    console.log('Component attached!');
+  },
+    tick: function () {
+this.el.object3D.position.z=this.el.object3D.position.z+this.data.speed;   }
+});
